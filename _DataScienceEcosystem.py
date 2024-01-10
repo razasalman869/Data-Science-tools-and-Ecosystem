37da05a86286ade9a668a5e5e78fcab2ea72c77f
razasalman869 <155820792+razasalman869@users.noreply.github.com>
2024-01-10 20:08:46 +0500

    Add files via upload

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List popular languages for Data Science
+# - list data science libraries
+# - Table of Data Science tools
+# - Convert minutes to hours
+
+# #### Popular languages for Data Scientists 
+
+# - Python
+# - SQL
+# - R
+# - Scala
+
+# ####  Commonly used libraries used by Data Scientists:
+
+# **Python Libraries:**
+# - Pandas
+# - Numpy
+# - Matplotlib
+# - Scikit-learn
+# - Pytorch
+
+# **Scala Libraries:**
+# - Vegas
+# 
+
+# **R libraries:**
+# - ggplot2
+
+# | Data Science tools |
+# -----------------------------
+# | Rstudio | 
+# | Jupyter| 
+# | Ray |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# - **, For Exponential 
+# - //, For Integer Division 
+# - %,  For Modulus Operator
+# - +,  For Addition
+# - -,  For Subraction
+# 
+# 
+
+# In[5]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[7]:
+
+
+200//60
+
+
+# ## Author
+# Suleman Raza
